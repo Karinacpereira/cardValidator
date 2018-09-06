e400a8ff60864802982712b59e81c150a9919580
Karina Pereira <karina.biologa@gmail.com>
2018-09-06 01:02:26 -0300

    Lib com ESlint

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 function cardValidator (cardNumber){
-  // console.log(cardNumber);
-  if(cardNumber === undefined){
-    throw new Error ("Necessário inserir os números para verificação");
-  } else if (typeof cardNumber !== "number") {
-    throw new Error ("Necessário inserir os números para verificação");
-  } else if(cardNumber.toString().length < 2){
-    throw new Error ("Necessário mais de 1 dígito para verificação");
-  } else {
-    return isValidCard (cardNumber);
-  }
+    // console.log(cardNumber);
+    if(cardNumber === undefined){
+        throw new Error ("Necessário inserir os números para verificação");
+    } else if (typeof cardNumber !== "number") {
+        throw new Error ("Necessário inserir os números para verificação");
+    } else if(cardNumber.stringNumber().length < 2){
+        throw new Error ("Necessário mais de 1 dígito para verificação");
+    } else {
+        return isValidCard (cardNumber);
+    }
 }
 
 function isValidCard(cardNumber){
-  // console.log(cardNumber)
-  var stringNumber = cardNumber.toString().split("");
-  var arrayReverse = [];
-  var arrayNumber = [];
-  var sum = 0;
+    // console.log(cardNumber)
+    let stringNumber = cardNumber.stringNumber().split("");
+    let arrayReverse = [];
+    let arrayNumber = [];
+    let sum = 0;
   
-  // console.log(stringNumber);
-  for (var i = stringNumber.length - 1; i >= 0; i--){
-    arrayReverse.push(stringNumber[i]);
+    // console.log(stringNumber);
+    for (var i = stringNumber.length - 1; i >= 0; i--){
+        arrayReverse.push(stringNumber[i]);
     // console.log(arrayReverse);
-  }
-
-  for (var j = 0; j < arrayReverse.length; j++){
-    if (j % 2 === 1){
-      var multiplication = arrayReverse[j] * 2;
-      if (multiplication >= 10){
-        var subtraction = multiplication - 9;
-        arrayNumber.push(subtraction);
-      } else {
-        arrayNumber.push(multiplication);
-      }
-    } else {
-      arrayNumber.push(parseInt(arrayReverse[j]));
-      // console.log(arrayNumber);
     }
-  }
 
-  for (var k = 0; k < arrayNumber.length; k++){
-    sum += arrayNumber[k];
+    for (var j = 0; j < arrayReverse.length; j++){
+        if (j % 2 === 1){
+            let multiplication = arrayReverse[j] * 2;
+            if (multiplication >= 10){
+                let subtraction = multiplication - 9;
+                arrayNumber.push(subtraction);
+            } else {
+                arrayNumber.push(multiplication);
+            }
+        } else {
+            arrayNumber.push(parseInt(arrayReverse[j]));
+            // console.log(arrayNumber);
+        }
+    }
+
+    for (var k = 0; k < arrayNumber.length; k++){
+        sum += arrayNumber[k];
     // console.log(sum);
-  }
+    }
 
-  // return sum % 10 === 0 && sum !== 0
+    // return sum % 10 === 0 && sum !== 0
 
-  if (sum % 10 === 0 && sum !== 0){
-    return true;
-  } else {
-    return false;
-  }
+    if (sum % 10 === 0 && sum !== 0){
+        return true;
+    } else {
+        return false;
+    }
 }
 
-module.exports.cardValidator = cardValidator;
-module.exports.isValidCard = isValidCard;
+const _cardValidator = cardValidator;
+export { _cardValidator as cardValidator };
